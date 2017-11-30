159953819ae3b98f1c5962e3a7047c94cac444ae
Daniel Nathan <danielzipzip@hotmail.com>
2017-11-30 03:54:34 -0300

    primeira tela feita, ouvinte criado e botoes associados ao mesmo, botao sair ja funcional

--- src/interfacegrafica/JFMenu.java
+++ src/interfacegrafica/JFMenu.java
@@ -3,16 +3,17 @@ package interfacegrafica;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
-
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import ouvintes.Ouvintebt1;
+
 public class JFMenu extends JFrame{
 	
+	private Ouvintebt1 ouvintebt = new Ouvintebt1();
 	private JPanel panel = new JPanel();
 	private JButton btvenda, btresgitro, btconsulta, btsair;
 	private JLabel lbTitulo;
@@ -54,16 +55,33 @@ public class JFMenu extends JFrame{
 	}
 	private void criarButoes(Container c){
 		
+		//botoes
 		this.btvenda = new JButton("Venda");
 		this.btresgitro = new JButton("Registro");
 		this.btconsulta = new JButton("Consulta");
 		this.btsair = new JButton("Sair");
 		
+		//largura e lugar onde sera posicionado
 		this.btvenda.setBounds(20, 230, 100, 30);
 		this.btresgitro.setBounds(20, 280, 100, 30);
 		this.btconsulta.setBounds(20, 330, 100, 30);
-		this.btsair.setBounds(20, 530, 100, 30);
+		this.btsair.setBounds(20, 520, 100, 30);
 		
+		//adicionando o ouvinte aos botoes
+		this.btvenda.addActionListener(this.ouvintebt);
+		this.btresgitro.addActionListener(this.ouvintebt);
+		this.btconsulta.addActionListener(this.ouvintebt);
+		this.btsair.addActionListener(this.ouvintebt);
+		
+		//definindo comandos associado
+		
+		this.btvenda.setActionCommand("venda");
+		this.btresgitro.setActionCommand("registro");
+		this.btconsulta.setActionCommand("consulta");
+		this.btsair.setActionCommand("sair");
+		
+		
+		//adicionando os botoes ao container
 		c.add(this.btvenda);
 		c.add(this.btresgitro);
 		c.add(this.btconsulta);
@@ -74,10 +92,9 @@ public class JFMenu extends JFrame{
 	}
 	private void criarlabel(Container c){
 		this.lbTitulo = new JLabel("MENU PRINCIPAL");
-		this.lbTitulo.setBounds(20, 230, 100, 30);
-		this.lbTitulo.setFont(new Font("Arial", Font.BOLD, 400));
-		this.lbTitulo.setBackground(Color.BLACK);
-		this.lbTitulo.setForeground(Color.BLUE);
+		this.lbTitulo.setBounds(300, 20, 200, 20);
+		this.lbTitulo.setFont(new Font("Arial", Font.BOLD, 20));
+		this.lbTitulo.setForeground(Color.BLACK);
 		this.lbTitulo.setToolTipText("MENU PRINCIPAL");
 		c.add(lbTitulo);
 	}
